a478adeb9c384b448fe92376df3bed9255c29f49
akkanour <akkaa01001@gmail.com>
2024-11-28 15:46:05 +0100

    update contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,21 +1,73 @@
-import ContactForm from "@components/ContactForm";
-import PrivacyModal from "@components/PrivacyModal";
-import { useState } from "react";
-
 
 export default function Contact() {
-  const [isPrivacyModalOpen, setPrivacyModalOpen] = useState(false);
-
-  const openPrivacyModal = () => setPrivacyModalOpen(true);
-  const closePrivacyModal = () => setPrivacyModalOpen(false);
-
   return (
     <div className="container mx-auto px-6 md:px-16 lg:px-32 xl:px-44">
-      {/* Formulaire de contact */}
-      <ContactForm openPrivacyModal={openPrivacyModal} />
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Contactez-nous</h2>
+        <p className="mt-2 text-lg text-gray-600">Nous sommes à votre disposition pour toute information supplémentaire.</p>
+      </div>
 
-      {/* Modal de politique de confidentialité */}
-      {isPrivacyModalOpen && <PrivacyModal closePrivacyModal={closePrivacyModal} />}
+      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label htmlFor="first-name" className="block text-sm font-semibold text-gray-900">Prénom</label>
+            <div className="mt-2.5">
+              <input id="first-name" name="first-name" type="text" autoComplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="last-name" className="block text-sm font-semibold text-gray-900">Nom</label>
+            <div className="mt-2.5">
+              <input id="last-name" name="last-name" type="text" autoComplete="family-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="company" className="block text-sm font-semibold text-gray-900">Société</label>
+            <div className="mt-2.5">
+              <input id="company" name="company" type="text" autoComplete="organization" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm font-semibold text-gray-900">Email</label>
+            <div className="mt-2.5">
+              <input id="email" name="email" type="email" autoComplete="email" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="phone-number" className="block text-sm font-semibold text-gray-900">Numéro de téléphone</label>
+            <div className="mt-2.5 relative">
+              <input id="phone-number" name="phone-number" type="tel" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="block text-sm font-semibold text-gray-900">Message</label>
+            <div className="mt-2.5">
+              <textarea id="message" name="message" rows={4} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            </div>
+          </div>
+
+          <div className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+              <input type="checkbox" className="h-4 w-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" />
+            </div>
+            <label htmlFor="consent" className="text-sm text-gray-600">
+              En sélectionnant ceci, vous acceptez notre{' '}
+              <button type="button"  className="font-semibold text-indigo-600 underline">politique de confidentialité</button>.
+            </label>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Discutons
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
