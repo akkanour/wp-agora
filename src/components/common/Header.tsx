de0704b82cdeb964c8ecff74be7c7f84e6f4047e
akkanour <akkaa01001@gmail.com>
2024-12-12 16:10:37 +0100

    update header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,44 +2,11 @@
 'use client';
 
 import { useState } from 'react';
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  PopoverGroup,
-} from '@headlessui/react';
+import { Dialog, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel, PopoverGroup} from '@headlessui/react';
 import { Link, useLocation } from 'react-router-dom';
 import { ChevronDownIcon, XMarkIcon, Bars3Icon } from '@heroicons/react/20/solid';
 
-const menuItems = [
-  { name: 'Accueil', href: '/' },
-  {
-    name: 'À propos',
-    subMenu: [
-      { name: 'Qui sommes-nous ?', href: '/a-propos/qui-sommes-nous' },
-      { name: 'Nos Produits', href: '/a-propos/nos-produits' },
-      { name: 'Nos Références', href: '/a-propos/nos-references' },
-    ],
-  },
-  {
-    name: 'Services',
-    subMenu: [
-      { name: 'Intervention sur Incident', href: '/services/intervention-sur-incident' },
-      { name: 'Contrat de support et Maintenance', href: '/services/contrat-support-maintenance' },
-      { name: 'Infogérance Globale ou Partielle', href: '/services/infogerance-globale-partielle' },
-      { name: 'Ressource en Régie', href: '/services/ressource-en-regie' },
-      { name: 'Intégration ERP', href: '/services/integration-erp' },
-    ],
-  },
-  {
-    name: 'Consulting',
-    href: '/consulting',
-  },
-  { name: 'Solutions', href: '/solutions' },
-  { name: 'Contact', href: '/contact' },
-];
+import { menuItems } from '@components/common/menuItems';
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -60,20 +27,20 @@ export default function Header() {
   return (
     <>
       <header
-        className="relative top-0 w-full bg-secondary-white text-[#23374D] z-[1040]">
+        className="relative top-0 w-full bg-secondary-white text-primary-dark font-title text-sm z-[1040]">
 
-        <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4">
+        <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-2.5">
           <div className="flex lg:flex-1">
             <Link to="/" className="" onClick={() => handleLinkClick(null)}>
               <span className="sr-only">Your Company</span>
-              <img alt="Logo" src="/logos-AT/AT-1.png" className="h-16 lg:h-20 w-auto" />
+              <img alt="Logo" src="/logos-AT/AT-1.png" className="h-16  w-auto" />
             </Link>
           </div>
           <div className="flex lg:hidden">
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-[#23374D]"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
             >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="h-6 w-6" />
@@ -86,27 +53,27 @@ export default function Header() {
                   <>
                     <button
                       onClick={() => handleMenuClick(item.name)}
-                      className={`flex items-center gap-x-1 text-sm font-semibold leading-6 ${selectedMenu === item.name ? 'text-[#024CAA]' : 'text-[#23374D]'}`}
+                      className={`flex items-center gap-x-1 leading-6 ${selectedMenu === item.name ? 'text-primary-lightBlue' : 'text-primary-dark'}`}
                     >
                       {item.name}
-                      <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-[#024CAA]" />
+                      <ChevronDownIcon aria-hidden="true" className={"h-5 w-5 flex-none ${selectedMenu === item.name ? 'text-primary-lightBlue' : 'text-[#23374D]'}"} />
                       <span
-                        className={`absolute bottom-[-4px] left-0 h-[2px] transition-all duration-500 ${selectedMenu === item.name || openSubMenu === item.name
-                          ? 'w-full bg-[#024CAA]'
-                          : 'w-0 group-hover:w-full bg-[#024CAA]'
+                        className={`absolute bottom-[0px] left-0 h-[2px] transition-all duration-500 ${selectedMenu === item.name || openSubMenu === item.name
+                          ? 'w-full bg-primary-lightBlue'
+                          : 'w-0 group-hover:w-full bg-primary-lightBlue'
                           }`}
                       ></span>
                     </button>
                     {openSubMenu === item.name && (
-                      <div className="absolute z-10 mt-3 min-w-[200px] max-w-xs overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                        <div className="p-4">
+                      <div className="absolute z-10 mt-3 min-w-[200px] max-w-xs overflow-hidden rounded-lg bg-white shadow-lg">
+                        <div className="p-2.5">
                           {item.subMenu.map((subItem) => (
                             <Link
                               key={subItem.name}
                               to={subItem.href}
-                              className={`block px-4 py-2 text-sm hover:bg-[#024CAA] hover:text-white whitespace-nowrap ${location.pathname === subItem.href
-                                ? 'border-l-4 border-[#024CAA] bg-[#E5E5E5]'
-                                : 'text-[#23374D]'
+                              className={`block p-2 hover:bg-primary-lightBlue hover:text-white whitespace-nowrap ${location.pathname === subItem.href
+                                ? 'border-l-4 border-primary-lightBlue '
+                                : 'text-primary-darkBlue'
                                 }`}
                               onClick={() => handleLinkClick(item.name)}
                             >
@@ -120,12 +87,12 @@ export default function Header() {
                 ) : (
                   <Link
                     to={item.href}
-                    className="text-sm font-semibold leading-6 text-[#23374D] relative"
+                    className={`leading-6 ${selectedMenu === item.name ? 'text-primary-lightBlue' : 'text-[#23374D]'} relative`}
                     onClick={() => handleLinkClick(item.name)}
                   >
                     {item.name}
                     <span
-                      className={`absolute bottom-[-4px] left-0 h-[2px] transition-all duration-500 ${selectedMenu === item.name ? 'w-full bg-[#024CAA]' : 'w-0 group-hover:w-full bg-[#024CAA]'}`}
+                      className={`absolute bottom-[-3px] left-0 h-[2px] transition-all duration-500 ${selectedMenu === item.name ? 'w-full bg-primary-lightBlue' : 'w-0 group-hover:w-full bg-primary-lightBlue'}`}
                     ></span>
                   </Link>
                 )}
@@ -168,7 +135,7 @@ export default function Header() {
                             <Link
                               key={subItem.name}
                               to={subItem.href}
-                              className="block rounded-lg py-2 text-sm font-semibold leading-7 text-[#23374D] hover:bg-[#024CAA] hover:text-white"
+                              className="block rounded-lg py-2 text-sm font-semibold leading-7 text-[#23374D] hover:bg-primary-lightBlue hover:text-white"
                               onClick={() => handleLinkClick(item.name)}
                             >
                               {subItem.name}
---
+++ src/components/common/menuItems.ts
@@ -0,0 +1,27 @@
+export const menuItems = [
+    { name: 'Accueil', href: '/' },
+    {
+      name: 'À propos',
+      subMenu: [
+        { name: 'Qui sommes-nous ?', href: '/a-propos/qui-sommes-nous' },
+        { name: 'Nos Produits', href: '/a-propos/nos-produits' },
+        { name: 'Nos Références', href: '/a-propos/nos-references' },
+      ],
+    },
+    {
+      name: 'Services',
+      subMenu: [
+        { name: 'Intervention sur Incident', href: '/services/intervention-sur-incident' },
+        { name: 'Contrat de support et Maintenance', href: '/services/contrat-support-maintenance' },
+        { name: 'Infogérance Globale ou Partielle', href: '/services/infogerance-globale-partielle' },
+        { name: 'Ressource en Régie', href: '/services/ressource-en-regie' },
+        { name: 'Intégration ERP', href: '/services/integration-erp' },
+      ],
+    },
+    {
+      name: 'Consulting',
+      href: '/consulting',
+    },
+    { name: 'Solutions', href: '/solutions' },
+    { name: 'Contact', href: '/contact' },
+  ];
